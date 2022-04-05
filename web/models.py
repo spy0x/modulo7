096f7441dd93a1153853f66088dcfc2f3775c712
Francisco V. Diaz <fevarga1@uc.cl>
2022-04-05 12:37:55 -0400

    starting to add models.py

--- web/models.py
+++ web/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Region(models.Model):
+    id = models.IntegerField(primary_key=True, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+
+class Comuna(models.Model):
+    #se omite el id para que la ORM lo genere automaticamente
+    name = models.CharField(max_length=50, null=False, blank=False)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+class UserType(models.Model):
+    type = models.CharField(max_length=25, primary_key=True)
+    canAdmin = models.BooleanField(default=False)
+
+class User(models.Model):
+    pass
