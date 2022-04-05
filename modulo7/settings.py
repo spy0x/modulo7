90601e542d5099e00a0587bb51bfdc202702b226
Francisco V. Diaz <fevarga1@uc.cl>
2022-04-05 11:47:36 -0400

    added app to project, added database config to settings

--- modulo7/settings.py
+++ modulo7/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,14 @@ WSGI_APPLICATION = 'modulo7.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME' : 'modulo7',
+		'USER' : 'modulo7',
+		'PASSWORD' : 'modulo7',
+		'HOST' : 'localhost',
+		'PORT' : '5432' 
+	}
 }
 
 
