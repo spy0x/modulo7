34e216031c71a05f1a47a57ed5d32e80269383ad
Francisco V. Diaz <fevarga1@uc.cl>
2022-04-05 13:36:32 -0400

    created inmuble y user models. not done all yet

--- web/models.py
+++ web/models.py
@@ -1,8 +1,9 @@
+from random import choices
 from django.db import models
 
 # Create your models here.
 class Region(models.Model):
-    id = models.IntegerField(primary_key=True, null=False, blank=False)
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50, null=False, blank=False)
 
 class Comuna(models.Model):
@@ -15,4 +16,34 @@ class UserType(models.Model):
     canAdmin = models.BooleanField(default=False)
 
 class User(models.Model):
-    pass
+    rut = models.CharField(max_length=10, primary_key=True)
+    primer_nombre = models.CharField(max_length=50, null=False, blank=False)
+    segundo_nombre = models.CharField(max_length=50, default='')
+    primer_apellido = models.CharField(max_length=50, null=False, blank=False)
+    segundo_apellido = models.CharField(max_length=50, default='')
+    direccion = models.CharField(max_length=100, null=False, blank=False)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=False, blank=False)
+    comuna = models.ForeignKey(Comuna, on_delete=models.SET_NULL, null=False, blank=False)
+    telefono = models.IntegerField(null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    user_type = models.ForeignKey(UserType, on_delete=models.SET_NULL, null=False, blank=False)
+
+class Inmueble(models.Model):
+    #se omite el id para que la ORM lo genere automaticamente    
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    descripcion = models.TextField(null=False, blank=False)
+    m2_construidos = models.IntegerField(null=False, blank=False)
+    m2_totales = models.IntegerField(null=False, blank=False)
+    n_estacionamientos = models.IntegerField(null=False, blank=False)
+    n_habitaciones = models.IntegerField(null=False, blank=False)
+    n_banos = models.IntegerField(null=False, blank=False)
+    direccion = models.CharField(max_length=100, null=False, blank=False)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=False, blank=False)
+    comuna = models.ForeignKey(Comuna, on_delete=models.SET_NULL, null=False, blank=False)
+    TIPO_CHOICES = [
+        ('C', 'Casa'),
+        ('D', 'Departamento'),
+        ('P', 'Parcela'),
+    ]
+    tipo = models.CharField(max_length=1, choices=TIPO_CHOICES, default='C')
+    precio_mensual = models.IntegerField(null=False, blank=False)
