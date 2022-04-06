795f9287d42740564c07f5432df25675e5a136d3
Francisco V. Diaz <fevarga1@uc.cl>
2022-04-06 10:10:46 -0400

    models done

--- web/models.py
+++ web/models.py
@@ -1,4 +1,3 @@
-from random import choices
 from django.db import models
 
 # Create your models here.
@@ -9,11 +8,14 @@ class Region(models.Model):
 class Comuna(models.Model):
     #se omite el id para que la ORM lo genere automaticamente
     name = models.CharField(max_length=50, null=False, blank=False)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.PROTECT)
 
 class UserType(models.Model):
     type = models.CharField(max_length=25, primary_key=True)
-    canAdmin = models.BooleanField(default=False)
+    can_admin = models.BooleanField(default=False) #Campo que potencialmente podría usarse para crear un Superusuario con seccion especial
+    can_list_inmuebles = models.BooleanField(default=True) #Para que los arrendatarios puedan listar propiedades por comuna
+    can_request_inmuebles = models.BooleanField(default=True) #Para que los arrendatarios puedan solicitar inmubles
+    can_list_propiedades = models.BooleanField(default=False) #Para que los arrendadores puedan listar sus propiedades en su dashboard
 
 class User(models.Model):
     rut = models.CharField(max_length=10, primary_key=True)
@@ -22,29 +24,35 @@ class User(models.Model):
     primer_apellido = models.CharField(max_length=50, null=False, blank=False)
     segundo_apellido = models.CharField(max_length=50, default='')
     direccion = models.CharField(max_length=100, null=False, blank=False)
-    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=False, blank=False)
-    comuna = models.ForeignKey(Comuna, on_delete=models.SET_NULL, null=False, blank=False)
+    region = models.ForeignKey(Region, on_delete=models.PROTECT)
+    comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT)
     telefono = models.IntegerField(null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
-    user_type = models.ForeignKey(UserType, on_delete=models.SET_NULL, null=False, blank=False)
+    user_type = models.ForeignKey(UserType, on_delete=models.PROTECT)
 
 class Inmueble(models.Model):
     #se omite el id para que la ORM lo genere automaticamente    
     nombre = models.CharField(max_length=50, null=False, blank=False)
-    descripcion = models.TextField(null=False, blank=False)
+    descripcion = models.TextField(max_length=255, null=False, blank=False)
     m2_construidos = models.IntegerField(null=False, blank=False)
     m2_totales = models.IntegerField(null=False, blank=False)
     n_estacionamientos = models.IntegerField(null=False, blank=False)
     n_habitaciones = models.IntegerField(null=False, blank=False)
     n_banos = models.IntegerField(null=False, blank=False)
     direccion = models.CharField(max_length=100, null=False, blank=False)
-    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=False, blank=False)
-    comuna = models.ForeignKey(Comuna, on_delete=models.SET_NULL, null=False, blank=False)
+    region = models.ForeignKey(Region, on_delete=models.PROTECT)
+    comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT)
     TIPO_CHOICES = [
-        ('C', 'Casa'),
-        ('D', 'Departamento'),
-        ('P', 'Parcela'),
+        ('Casa', 'Casa'),
+        ('Departamento', 'Departamento'),
+        ('Parcela', 'Parcela'),
     ]
-    tipo = models.CharField(max_length=1, choices=TIPO_CHOICES, default='C')
+    tipo = models.CharField(max_length=15, choices=TIPO_CHOICES, default='C')
     precio_mensual = models.IntegerField(null=False, blank=False)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    arrendador = models.ForeignKey(User, on_delete=models.CASCADE, related_name='arrendador')
+    arrendatario = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='arrendatario', null=True, blank=True)
+
+class SolicitudArriendo(models.Model):
+    #se omite el id para que la ORM genere automaticamente
+    user_request = models.ForeignKey(User, on_delete=models.CASCADE)
+    inmueble_request = models.ForeignKey(Inmueble, on_delete=models.CASCADE)
