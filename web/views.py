91f971b354f46551e2936c004a87065b9a5228d3
Francisco V. Diaz <fevarga1@uc.cl>
2022-04-11 11:43:14 -0400

    created views for app web

--- web/views.py
+++ web/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from web.models import *
+from web.forms import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+def RegisterView(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/register_tipo?user=' + str(form.cleaned_data['username']))
+    else:
+        form = UserForm()
+        
+    context = {
+        'form': form, 
+    }
+    return render(request, 'registration/register.html', context)
 
 # Create your views here.
