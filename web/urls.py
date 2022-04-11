2dabe389ca0410e4efe2284d8519c4dc3a181a39
Francisco V. Diaz <fevarga1@uc.cl>
2022-04-11 10:46:46 -0400

    added urls.py for project and app

---
+++ web/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('', views.IndexView, name='home'),
+    path('login/', LoginView.as_view(next_page='dashboard'), name='login_url'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('register/', views.RegisterView, name='register_url'),
+    path('register_tipo/', views.Register_TipoView, name='register_tipo_url'),
+    path('dashboard/', views.DashboardView, name='dashboard'),
+]
