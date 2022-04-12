ef17ce750de87f443af6a30ddf7cf14d75944a48
Francisco V. Diaz <fevarga1@uc.cl>
2022-04-12 13:31:27 -0400

    adjusted models. corrected json files. made migrations

--- web/models.py
+++ web/models.py
@@ -17,17 +17,12 @@ class InmuebleType(models.Model):
     type = models.CharField(max_length=25, primary_key=True)
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='usuario')
     rut = models.CharField(max_length=10, primary_key=True)
-    primer_nombre = models.CharField(max_length=50, null=False, blank=False)
-    segundo_nombre = models.CharField(max_length=50, default='')
-    primer_apellido = models.CharField(max_length=50, null=False, blank=False)
-    segundo_apellido = models.CharField(max_length=50, default='')
     direccion = models.CharField(max_length=100, null=False, blank=False)
     region = models.ForeignKey(Region, on_delete=models.PROTECT)
     comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT)
     telefono = models.IntegerField(null=False, blank=False)
-    email = models.EmailField(null=False, blank=False)
     user_type = models.ForeignKey(UserType, on_delete=models.PROTECT)
 
 class Inmueble(models.Model):
@@ -44,10 +39,10 @@ class Inmueble(models.Model):
     comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT)
     tipo = models.ForeignKey(InmuebleType, on_delete=models.PROTECT)
     precio_mensual = models.IntegerField(null=False, blank=False)
-    arrendador = models.ForeignKey(User, on_delete=models.CASCADE, related_name='arrendador')
-    arrendatario = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='arrendatario', null=True, blank=True)
+    arrendador = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='arrendador')
+    arrendatario = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, related_name='arrendatario', null=True, blank=True)
 
 class SolicitudArriendo(models.Model):
     #se omite el id para que la ORM genere automaticamente
-    user_request = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_request = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     inmueble_request = models.ForeignKey(Inmueble, on_delete=models.CASCADE)
